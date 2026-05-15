192568d25afaa88dcc2d55771799c7b5e92b8bf4
FacuMint <facundolemo@gmail.com>
2026-05-14 23:07:10 -0300

    14/05

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
 from typing import Annotated
-from fastapi import FastAPI, Path, Query, HTTPException
+
+from fastapi import FastAPI, HTTPException, Path, Query
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 app.title = "Mi primera API"  # Así cambia el nombre en /docs
 
+# Request -> Middleware -> Path Operation -> Middleware -> Response
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",  # entorno desarrollo
+        "https://faculemo.github.io/front",  # entorno producción
+        # "*", wildcard ! Cualquier origen
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 # METODOS: 2 GET, POST PUT DELETE
+# DRY : DON'T REPEAT YOURSELF
+
+not_found = {
+    404: {
+        "description": "Response not found si no se encuentra el id",
+        "content": {
+            "application/json": {
+                "example": {
+                    "detail": "Artículo no encontrado",
+                }
+            }
+        },
+    },
+}
+
 
 STR_CORTITO = Annotated[str, Field(max_length=30)]
 PRECIO_PVP = Annotated[float, Field(lt=999999)]
@@ -14,15 +46,17 @@ BOOL_ACTIVO = Annotated[bool, Field(description="Disponible?")]
 
 
 class ArticuloSchema(BaseModel):
-    id: Annotated[int, Field(gt=0, description="ID del articulo", deprecated=True)] 
+    id: Annotated[int, Field(gt=0, description="ID del articulo", deprecated=True)]
     nombre: STR_CORTITO
     precio: PRECIO_PVP = 1500
     activo: BOOL_ACTIVO = True
+
 
 class ArticuloUpdateSchema(BaseModel):
     nombre: STR_CORTITO
     precio: PRECIO_PVP = 2000
     activo: BOOL_ACTIVO = True
+
 
 # Simulación db supermercado:
 articulos = [
@@ -32,19 +66,65 @@ articulos = [
 ]
 
 
-@app.get("/articulos")
-async def get_articulos() -> list[ArticuloSchema]:
+@app.get("/articulos", response_model=list[ArticuloSchema])
+async def get_articulos():
     return articulos
 
 
-@app.get("/articulos/{id}")  # Parámetro de ruta (esta en la url)
+@app.get(
+    "/articulos/{id}",  # Parámetro de ruta (esta en la url)
+    responses=not_found,
+    response_model=ArticuloSchema,
+)
 async def get_articulos_by_id(
     id: Annotated[int, Path(gt=0)],
-) -> ArticuloSchema:
+):
     for articulo in articulos:
         if articulo["id"] == id:
             return articulo
+    raise HTTPException(status_code=404, detail="Artículo no encontrado")
+
+
+@app.post("/articulos", response_model=list[ArticuloSchema])
+async def crear_articulo(articulo_nuevo: ArticuloSchema):
+    articulos.append(articulo_nuevo.model_dump())
+    return articulos
+
+
+@app.delete(
+    "/articulos/{id}",  # ?logico=false
+    responses=not_found,
+    response_model=ArticuloSchema,
+)
+async def borrar_articulo(
+    id: Annotated[int, Path(gt=0)],
+    logico: Annotated[bool, Query(description="Mantener registro?")] = False,
+) -> ArticuloSchema:
+    for articulo in articulos:
+        if articulo["id"] == id:
+            if logico:
+                articulo["activo"] = (False,)
+            else:
+                articulos.remove(articulo)
+            return articulo
+    raise HTTPException(status_code=404, detail="Artículo no encontrado")
+
+
+@app.put("/articulos/{id}", responses=not_found, response_model=ArticuloSchema)
+async def editar_articulo(
+    id: Annotated[int, Path(gt=0, description="Id del producto. >0")],
+    articulo_editar: ArticuloUpdateSchema,
+):
+    for articulo in articulos:
+        if articulo["id"] == id:
+            articulo["nombre"] = articulo_editar.nombre
+            articulo["precio"] = articulo_editar.precio
+            articulo["activo"] = articulo_editar.activo
+            return articulo
     raise HTTPException(status_code=404, detail="Articulo no encontrado")
+
+
+"""
 
 # Parámetro query-> /articulos?clave=valor&llave=valor
 
@@ -61,44 +141,6 @@ async def get_articulos_by_id(
 # max_length
 
 
-@app.post("/articulos")  
-async def crear_articulo(
-    articulo_nuevo: ArticuloSchema
-) -> list[ArticuloSchema]:
-    articulos.append(articulo_nuevo.model_dump())
-    return articulos
-
-
-@app.delete("/articulos/{id}")  # ?logico=false
-async def borrar_articulo(
-    id: Annotated[int, Path(gt=0)],
-    logico: Annotated[bool, Query(description="Mantener registro?")] = False,
-) -> ArticuloSchema:
-    for articulo in articulos:
-        if articulo["id"] == id:
-            if logico:
-                articulo["activo"] = (False,)
-            else:
-                articulos.remove(articulo)
-            return articulo
-    raise HTTPException(status_code=404, detail="Articulo no encontrado")
-
-
-@app.put("/articulos/{id}")
-async def editar_articulo(
-    id: Annotated[int, Path(gt=0, description="Id del producto. >0")],
-    articulo_editar: ArticuloUpdateSchema
-) -> ArticuloSchema:
-    for articulo in articulos:
-        if articulo["id"] == id:
-            articulo["nombre"] = articulo_editar.nombre
-            articulo["precio"] = articulo_editar.precio
-            articulo["activo"] = articulo_editar.activo
-            return articulo
-    raise HTTPException(status_code=404, detail="Articulo no encontrado")
-
-
-"""
 @app.get("/saludo")  # "/Saludo" es el endpoint de la url
 async def saludo():
     return {"hola": "mundo"}
